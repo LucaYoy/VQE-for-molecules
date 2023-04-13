0de89691b597dfc9fdbfe5fb6130f89054e6e436
LucaYoy <lucaion781@gmail.com>
2023-04-13 13:13:42 +0300

    starting implementing other molecules

--- scripts/plots.py
+++ scripts/plots.py
@@ -4,32 +4,33 @@ import QuantumCircuit as qc
 import ExactSolution as ex
 from matplotlib import pyplot as plt
 
-def plotOptimisation(R,energy_listFOGD,energy_listSOGD,energy_listPS,energy_listSPSA,iterations,layers,i=''):
+def plotOptimisation(R,energy_listFOGD,energy_listSOGD,energy_listPS,energy_listSPSA,iterations,layers,IBM_dict={'energyArray':[],'method':''},i=''):
 	fig, ax = plt.subplots()
 	ax.plot([0,iterations],[ex.exactEnergy(R),ex.exactEnergy(R)],'--',color='k',label='Exact')
 	ax.plot(energy_listFOGD,'bo-',label='VQE-FirstOrderGD')
 	ax.plot(energy_listSOGD,'ro-',label='VQE-SecondOrderGD')
 	ax.plot(energy_listPS,'go-',label='VQE-ParameterShiftGD')
 	ax.plot(energy_listSPSA,'ko-',label='VQE-SPSA')
+	ax.plot(IBM_dict['energyArray'],'o-',label=f"IBM-VQE-{IBM_dict['method']}")
 	ax.set(xlabel = 'iterations', ylabel = 'energy')
 	ax.legend()
 	plt.show()
 	fig.set_size_inches(16,12)
-	fig.savefig(f'../plots/{R}optimizationPlotWith{layers}Layers{i}.png',format='png',dpi=100)
+	fig.savefig(f'../plots/{R}optimizationPlotWith{layers}Layers{i}.pdf',format='pdf',dpi=100)
 
 def plotEAgainstR(RArray,exactE,approxESim,layers,method,approxE_IBM=None):
 	fig, ax = plt.subplots()
 	ax.plot(RArray,exactE,'k-',label='Exact')
 	ax.plot(RArray,approxESim,'bx-',label='VQE_Simulation')
 	if approxE_IBM:
-		ax.plot(RArray,approxE_IBM,'rx-',label='VQE_IBM')
+		ax.plot(approxE_IBM[0],approxE_IBM[1],'rx',label='VQE_IBM')
 
 	ax.set_xlabel('R')
 	ax.set_ylabel('E')
 	ax.legend()
 	plt.show()
 	fig.set_size_inches(16,12)
-	fig.savefig(f'../plots/Eplot_{method}_{layers}Layers.png',format='png',dpi=100)
+	fig.savefig(f'../plots/Eplot_{method}_{layers}Layers.pdf',format='pdf',dpi=100)
 
 def plotDeltaE_layers(R,approxEArray,exactE,method):
 	fig, ax = plt.subplots()
@@ -39,7 +40,7 @@ def plotDeltaE_layers(R,approxEArray,exactE,method):
 
 	plt.show()
 	fig.set_size_inches(16,12)
-	fig.savefig(f'../plots/deltaE_layers_{R}_{method}.png',format='png',dpi=100)
+	fig.savefig(f'../plots/deltaE_layers_{R}_{method}.pdf',format='pdf',dpi=100)
 
 
 
